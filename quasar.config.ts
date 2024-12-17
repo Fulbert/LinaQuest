548f6bd12c802526426a15caaced38d9f90983e8
Grégoire Lannoy <gregoire.lannoy@bobst.com>
2024-12-17 13:20:43 +0100

    Try fix deploy

--- quasar.config.ts
+++ quasar.config.ts
@@ -44,14 +44,14 @@ export default defineConfig((ctx) => {
         // extendTsConfig (tsConfig) {}
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/LinasQuest',
       // analyze: true,
       // env: {},
       // rawDefine: {}
